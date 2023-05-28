d93ac31534aaa3c792c957478edd3a50cccf6acb
dvvteja <dvvteja@gmail.com>
2023-05-28 21:57:59 +0530

    Factorial File Added

---
+++ src/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String args[]) {
+		int a;
+		System.out.print("Enter a number:");
+		try (Scanner sc = new Scanner(System.in)) {
+			a=sc.nextInt();
+		}
+		System.out.println("Factorial of "+a+" :"+Fact(a));
+	}
+	public static int Fact(int n) {
+		if(n==0 || n==1)
+			return 1;
+		else
+			return n*Fact(n-1);
+	}
+
+}
