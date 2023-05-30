e1fdf8be7da818ea6686a5c2d65fae1d796e41b3
dvvteja <dvvteja@gmail.com>
2023-05-30 14:20:27 +0530

    Bubble Sort Updated

--- src/Bubble_Sort.java
+++ src/Bubble_Sort.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
 
 public class Bubble_Sort {
 	public static void main(String args[]) {
-		int a[]= {5,4,3,2,1};
-		for(int i=0;i<a.length;i++) {
-			for(int j=i+1;j<=i;j++) {
-				if(a[i]>a[j]) {
-					int tmp=a[j];
-					a[j]=a[j+1];
-					a[j+1]=tmp;
+		int i,j;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter Array Size:");
+			int n=sc.nextInt();
+			int a[]=new int[n];
+			for(i=0;i<n;i++) {
+				a[i]=sc.nextInt();
+			}		
+			for(i=0;i<n;i++) {
+				for(j=0;j<n-1;j++) {
+					if(a[j]>a[j+1]) {
+						int tmp=a[j];
+						a[j]=a[j+1];
+						a[j+1]=tmp;
+					}
 				}
 			}
+			System.out.println("Array Elements are:");
+			for(i=0;i<n;i++) {
+				System.out.println(a[i]);
+			}
 		}
-		System.out.println(a);
 	}
 }
