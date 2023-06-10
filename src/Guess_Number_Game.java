57774b08fdc6df6de25c051cf38e09bec8b8dc97
dvvteja <dvvteja@gmail.com>
2023-06-10 07:51:48 +0530

    Guess_Number_Game Updated

--- src/Guess_Number_Game.java
+++ src/Guess_Number_Game.java
@@ -7,14 +7,15 @@ public class Guess_Number_Game {
 		System.out.println("Random number will be choosen from 1 to 100. Guess the number in 5 trails.");
 		for (i = 1; i <= k; i++) {
 			System.out.println("Guess the number:");
-			Scanner sc = new Scanner(System.in);
-			int n = sc.nextInt();
-			if (n == num) {
-				System.out.println("Wow...Perfect Guess..!!");
-			} else if (n > num)
-				System.out.println(n + " is greater than the actual number.");
-			else if (n < num)
-				System.out.println(n + " is lesser than the actual number.");
+			try (Scanner sc = new Scanner(System.in)) {
+				int n = sc.nextInt();
+				if (n == num) {
+					System.out.println("Wow...Perfect Guess..!!");
+				} else if (n > num)
+					System.out.println(n + " is greater than the actual number.");
+				else if (n < num)
+					System.out.println(n + " is lesser than the actual number.");
+			}
 
 		}
 		if (i == k + 1) {
